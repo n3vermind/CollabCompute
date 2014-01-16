d4ad0bb6c31c4b0f6715d489fbec472fa30449f3
laiqu <laiquendir@gmail.com>
2014-01-16 17:54:34 +0100

    Reading peers from file.

--- Server.cpp
+++ Server.cpp
@@ -4,6 +4,7 @@ Server::Server(boost::asio::io_service &io, short port, std::string bootstrap) :
     acceptor(io, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)),
     socket(io), hash(Identify::getId())
 {
+	get_known_peers();
     if(bootstrap != "")
         std::cout << "Bootstraping to " << bootstrap << std::endl;
     accept();
@@ -18,4 +19,26 @@ void Server::accept()
                 std::make_shared<Connection>(std::move(socket))->start();
             accept();
         });
+}
+
+void Server::get_known_peers()
+{
+	
+	boost::filesystem::path peers_file("peers");
+	if(!boost::filesystem::exists(peers_file) || !boost::filesystem::is_regular_file(peers_file))
+		throw std::runtime_error("peers file is missing");
+	boost::filesystem::fstream peers_stream;
+	peers_stream.open(peers_file, boost::filesystem::fstream::in);
+	while(!peers_stream.eof()) 
+	{
+		char address[256];
+		peers_stream.getline(address,sizeof(address)/sizeof(char));
+		if(std::string(address).size() > 1)
+		{
+			std::cout << "Peer found in peers file: " << address << "\n";
+			peers.push_back(std::string(address));
+		}
+	}
+	if(peers.empty())
+		throw std::runtime_error("Couldn't find any peers in peers file\n");
 }
--- Server.hpp
+++ Server.hpp
@@ -1,6 +1,10 @@
 #include <string>
 #include <iostream>
+#include <vector>
+#include <stdexcept>
 #include <boost/asio.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
 
 #include "Connection.hpp"
 #include "Identify.hpp"
@@ -9,11 +13,14 @@ class Server
 {
     public:
         Server(boost::asio::io_service &io, short port, std::string bootstrap = "");
+		
         
     private:
         void accept();
+ 		void get_known_peers();
 
         std::string hash;
         boost::asio::ip::tcp::acceptor acceptor;
         boost::asio::ip::tcp::socket socket;
+		std::vector< std::string > peers;
 };
