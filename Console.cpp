b2db31b46a6494f2bca356c1f5f1cf9d3a601ff2
laiqu <laiquendir@gmail.com>
2014-03-13 18:58:07 +0100

    Basic console.

---
+++ Console.cpp
@@ -0,0 +1,36 @@
+#include "Console.hpp"
+
+Console::Console(boost::asio::io_service &io, Server* server) 
+	: server(server),
+	input(io, ::dup(STDIN_FILENO)),
+	output(io, ::dup(STDOUT_FILENO))
+{
+		boost::asio::async_read_until(input, input_buffer, '\n',
+				boost::bind(&Console::handle_read,this,
+					boost::asio::placeholders::error,
+					boost::asio::placeholders::bytes_transferred));
+}
+
+void Console::handle_read(const boost::system::error_code& error, std::size_t length)
+{
+	boost::asio::streambuf::const_buffers_type bufs = input_buffer.data();
+	std::string in(boost::asio::buffers_begin(bufs), 
+			boost::asio::buffers_begin(bufs) + input_buffer.size());
+	int choice = -1;
+	sscanf(in.c_str(),"%d",&choice);
+	switch(choice) {
+		case 0:
+			std::cout << server->get_next_hash() << std::endl;
+			break;
+	}
+	input_buffer.consume(input_buffer.size());
+	boost::asio::async_read_until(input, input_buffer, '\n',
+			boost::bind(&Console::handle_read,this,
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
+}
+
+void Console::handle_write(const boost::system::error_code& error)
+{
+
+}
---
+++ Console.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+
+#include "Server.hpp"
+
+class Server;
+
+class Console
+{
+	public:
+		Console(boost::asio::io_service &io, Server* server);
+	private:
+		Server* server;
+		boost::asio::posix::stream_descriptor input;
+		boost::asio::posix::stream_descriptor output;
+		boost::asio::streambuf input_buffer;
+		void handle_read(const boost::system::error_code& error, std::size_t length);
+		void handle_write(const boost::system::error_code& error);
+};
+
